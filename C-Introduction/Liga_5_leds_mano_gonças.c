f9a40a420b1a2fb1e5f633537ea26d48f7b6b64c
Gab Novalsky <99911394+gab5987@users.noreply.github.com>
2022-04-20 14:10:24 -0300

    optimized code

--- C-Introduction/Liga_5_leds_mano_gonças.c
+++ C-Introduction/Liga_5_leds_mano_gonças.c
@@ -3,33 +3,25 @@
 
 int main(){
   #define F_CPU 16000000UL
-  DDRB |= (1 << DDB7); // Set PB5 to output
-  DDRB |= (1 << DDB6);
-  DDRB |= (1 << DDB5);
-  DDRB |= (1 << DDB4);
+  for(int i = 4 ; i < 8 ; i++){
+    DDRB |= (1 << i);
+  }
   DDRH |= (1 << DDH5);
 
   // While loop turn on PB5 pin
   while (1) {
-    PORTB |= (1 << PORTB7); // Turn on PB5
-    _delay_ms(1000); // Delay 1 second
-    PORTB |= (1 << PORTB6); // Turn on PB5
-    _delay_ms(1000); // Delay 1 second
-    PORTB |= (1 << PORTB5); // Turn on PB5
-    _delay_ms(1000); // Delay 1 second
-    PORTB |= (1 << PORTB4); // Turn on PB5
-    _delay_ms(1000); // Delay 1 second
+   for(int i = 7 ; i > 3 ; i--){
+     PORTB |= (1 << i);
+    _delay_ms(1000);
+   }
     PORTH |= (1 << PH5); // Turn on PB5
     _delay_ms(1000); // Delay 1 second
 
-    PORTB &= ~(1 << PORTB7); // Turn off PB5 pin
-    _delay_ms(1000); // Delay 1 second
-    PORTB &= ~(1 << PORTB6); // Turn off PB5 pin
-    _delay_ms(1000); // Delay 1 second
-    PORTB &= ~(1 << PORTB5); // Turn off PB5 pin
-    _delay_ms(1000); // Delay 1 second
-    PORTB &= ~(1 << PORTB4); // Turn off PB5 pin
-    _delay_ms(1000); // Delay 1 second
+
+    for(int i = 7 ; i > 3 ; i--){
+     PORTB &= ~(1 << i);
+    _delay_ms(1000);
+   }
     PORTH &= ~(1 << PH5); // Turn off PB5 pin
     _delay_ms(1000); // Delay 1 second
   }
