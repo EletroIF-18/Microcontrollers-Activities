f328544ba085fd4b70d83ebaf6a55dcd101b0987
Gab Novalsky <99911394+gab5987@users.noreply.github.com>
2022-05-26 11:09:20 -0300

    add random matrix gen and start screen

--- src/asf.h
+++ src/asf.h
@@ -1,8 +1,9 @@
 #ifndef ASF_H
 #define ASF_H
 
-void randomize(unsigned char (*bolsonaro)[2][4][8], unsigned char (*lula)[2][4][8]);
 int compara_matriz(unsigned char (*bolsonaro)[2][4][8], unsigned char (*lula)[2][4][8]);
 void copy_pasta(int *pos_cursor, int *pos_copy, int *flag_copy, unsigned char (*lula)[2][4][8], int *con_mov);
+void genRandArrIndex(unsigned char (*base)[2][4][8], unsigned char (*randomized)[2][4][8], int seed);
+int start_screen();
 
 #endif // ASF_H
--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main(){
 	DDRC = 0x00; PORTC = 0xFF;
 	int em_ordem = 0, con_mov = 0, pos_cursor = 0x80;
 	int flag_copy=0, pos_copy;
+	int seed;
 
 	set_bit(PORTB, 2);
 	inic_LCD_4bits();
@@ -83,76 +84,7 @@ int main(){
 	};
 	
 	//MATRIZ COM OS CARACTERES NA POSIÇÃO RANDOMIZADA
-	unsigned char lula[2][4][8] = {
-		{// linha1
-		 {0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1E,
-		  0x18,
-		  0x10},
-		 {0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1C,
-		  0x10,
-		  0x03,
-		  0x0F,
-		  0x0F},
-		 {0x1F,
-		  0x1F,
-		  0x1F,
-		  0x07,
-		  0x01,
-		  0x18,
-		  0x1E,
-		  0x1F},
-		 {0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x0F,
-		  0x03,
-		  0x01}},
-
-		{// linha 2
-		 {0x10,
-		  0x18,
-		  0x1E,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F},
-		 {0x0F,
-		  0x07,
-		  0x03,
-		  0x10,
-		  0x1C,
-		  0x1F,
-		  0x1F,
-		  0x1F},
-		 {0x1E,
-		  0x1E,
-		  0x18,
-		  0x01,
-		  0x07,
-		  0x1F,
-		  0x1F,
-		  0x1F},
-		 {0x01,
-		  0x03,
-		  0x0F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F,
-		  0x1F}}
-
-	};
+	unsigned char lula[2][4][8];
 
 //====================================================//
 //    APRESENTA A IMAGEM AO USUARIO E ESPERA 5s       //
@@ -174,20 +106,31 @@ int main(){
 	for (int i = 0; i < 4; i++){ cmd_LCD(192 + i, 0); cmd_LCD(i + 4, 1);}
 
 	_delay_ms(1000);
+	clear_disp();
+	set_cursor(1,1); escreve_LCD("PRESS START");
+
+	while(tst_bit(PINC, 2))
+
+	while(!tst_bit(PINC, 2)){
+		seed++;
+		if(seed == 247){seed =0;}
+		//_delay_ms(1);
+	}
 
 //===========================================//
 // RANDOMIZA E ESCREVE OS VALORES NA MEMORIA //
 //===========================================//
-	randomize(&bolsonaro, &lula);
+	genRandArrIndex((&bolsonaro), (&lula), seed);
+	clear_disp();
 	//ESCREVE LINHA 1 RANDOMIZADA
 	for (int i = 0; i < 4; i++){ 
 		cmd_LCD(64 + 8 * i, 0);
-		for (int j = 0; j < 8; j++){cmd_LCD(bolsonaro[0][i][j], 1);}
+		for (int j = 0; j < 8; j++){cmd_LCD(lula[0][i][j], 1);}
 	}
 	//ESCREVE LINHA 2 RANDOMIZADA
 	for (int i = 0; i < 4; i++){
 		cmd_LCD(96 + 8 * i, 0);
-		for (int j = 0; j < 8; j++){cmd_LCD(bolsonaro[1][i][j], 1);}
+		for (int j = 0; j < 8; j++){cmd_LCD(lula[1][i][j], 1);}
 	}
 	//IMPRIME LINHA 1
 	for (int i = 0; i < 4; i++){ cmd_LCD(128 + i, 0); cmd_LCD(i, 1);}
@@ -225,6 +168,7 @@ int main(){
 				cmd_LCD(96 + 8 * i, 0);
 				for (int j = 0; j < 8; j++){cmd_LCD(lula[1][i][j], 1);}
 			}
+			clear_disp();
 			//IMPRIME LINHA 1
 			for (int i = 0; i < 4; i++){ cmd_LCD(128 + i, 0); cmd_LCD(i, 1);}
 			//IMPRIME LINHA 2
@@ -234,17 +178,14 @@ int main(){
 			_delay_ms(10);
 		}
 
-
-
-		em_ordem = compara_matriz(&bolsonaro, &lula); // SLA MAS NãO ESTá FUNCIONANDO
-
-		if(em_ordem == 1){         // NãO ESTá FUNCIONANDO
+		em_ordem = compara_matriz(&bolsonaro, &lula);
+		if(em_ordem == 1){
 			clear_disp();
 			set_cursor(1,1); escreve_LCD("Fim de jogo");
 			set_cursor(1,2); escreve_LCD("Voce venceu");
 			break;
 		}
-		if(con_mov > 16){
+		if(con_mov > 10){
 			clear_disp();
 			set_cursor(1,1); escreve_LCD("Fim de jogo");
 			set_cursor(1,2); escreve_LCD("Voce perdeu");
